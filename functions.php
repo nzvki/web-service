9f0de05b7db0773236e0142e5c82c2b0ccbfa3e8
nzvki <55402864+nzvki@users.noreply.github.com>
2019-10-06 21:19:10 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,25 @@
 <?php
  function get_price($find){
+  
  $phones=array(
- 
- 
- 
- 
- );
-
-
+  "note10"=>80000,
+  "iphone11"=>100000,
+  "oneplus7"=>50000,
+  "p30"=>75000,
+  "infinix"=>15000,
+  "s10"=>90000,
+   );
+  
+  foreach($phones as $phone=>$price){
+   
+   if($phone==$find)
+   {
+    return $price
+     break;
+   }
+   
+  }
+  
+ }
 
 ?>
