89eaa00a117a20248377c0288d044beefaf1ca43
nzvki <55402864+nzvki@users.noreply.github.com>
2019-10-06 22:26:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?php
 //process client request using URL
 
-if (!empty($GET['name']){
+header ("content-Type:application/xml");
+include("function.php");
+
+if (!empty($GET['name'])){
 //
-}
-else
-{
+  $name=$_GET['name'];
+  $price=get_price($name);
+  
+  if(empty($price)){
+    //phone not found
+    deliver_response(200,"phone not found",NULL);
+      
+    else
+      //phone price
+      deliver_response(200,"phone found, $price);
+      }
+
+else {
 //throw error(INVALID REQUEST)
+ deliver_response(400,"INVALID REQUEST",NULL)
 }
 
-
+function deliver_response($status,$status_message,$data){
+ header("HTTP/1.1 $status $status_message");
+  
+  $response['status']=$status;
+  $rsponse['status_message']=$status_message;
+  $response['data']=$data
+    
+  $xml_response=xml_encode ($response);
+  echo xml_response;
+  
+}
 ?>
